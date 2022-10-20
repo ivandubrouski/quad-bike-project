a7f23b1e6f757456e087e9a2bb27548dcaf1724d
Karasevgen1205 <karasevgen1205@yandex.ru>
2022-10-20 18:03:19 +0300

    lesson-8

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll(".routs__reserve"); // []
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".modal__close");
+const header = document.querySelector(".header");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -11,4 +12,20 @@ buttons.forEach((btn) => {
 
 close.addEventListener("click", () => {
   modal.classList.remove("show");
+});
+
+document.addEventListener("scroll", (event) => {
+  if (
+    event.target.scrollingElement.scrollTop &&
+    !header.classList.contains("header_white")
+  ) {
+    header.classList.add("header_white");
+  }
+
+  if (
+    !event.target.scrollingElement.scrollTop &&
+    header.classList.contains("header_white")
+  ) {
+    header.classList.remove("header_white");
+  }
 });
