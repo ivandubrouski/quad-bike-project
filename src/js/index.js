1fd1e81b17408b021c4e84da1970209e9769a76b
Karasevgen1205 <karasevgen1205@yandex.ru>
2022-11-17 20:26:04 +0300

    lesson-14

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,48 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    control: true,
+  },
+  spaceBetween: 40,
+  effect: "flip",
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
+
 /*******************MODAL************************ */
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -168,7 +213,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   let slideIndex = 1;
   const slides = document.querySelectorAll(".slider__slide");
-  const bullets = document.querySelectorAll(".slider__bullet");
+  const bullets = document.querySelectorAll(".pagination__bullet");
 
   showSlides(1);
 
